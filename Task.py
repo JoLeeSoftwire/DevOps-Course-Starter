158ba2bdd567b1fa2b5fa4fedd67fb1f90189b95
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-06-24 17:20:33 +0100

    module_2: Task class

---
+++ Task.py
@@ -0,0 +1,13 @@
+import enum
+
+class Status(enum.Enum):
+    ToDo = 0
+    Done = 1
+    # Doing = 2
+
+class Task:
+
+    def __init__(self, id, title, status=Status.ToDo):
+        self.id = id
+        self.title = title
+        self.state = status
