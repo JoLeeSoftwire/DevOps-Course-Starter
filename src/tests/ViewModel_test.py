6a4d581810446db481479d465cea157f1300b35d
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-07-22 19:54:28 +0100

    module3: red test older_done_tems

--- src/tests/ViewModel_test.py
+++ src/tests/ViewModel_test.py
@@ -14,7 +14,7 @@ def test_correct_recent_done_items():
     task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
     task5DoneAt = datetime.now() - timedelta(minutes=1)
     task6DoneAt = datetime.now() - timedelta(minutes=120)
-    expectedDone = [
+    expectedRecent = [
         Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
         Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
         Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=task5DoneAt),
@@ -37,10 +37,35 @@ def test_correct_recent_done_items():
     vm = ViewModel(inputTasks)
 
     # Then
-    assert vm.recent_done_items == expectedDone
+    assert vm.recent_done_items == expectedRecent
 
-# def test_show_all_if_more_than-five_and_toggle_on
+def test_show_all_if_more_than_five_and_toggle_on():
+    # Given
+    task1DoneAt = datetime.now() - timedelta(minutes=15)
+    task2DoneAt = datetime.now() - timedelta(minutes=10)
+    task3DoneAt = datetime.strptime("2020-01-08T08:15:27.243860Z", dateformat)
+    task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
+    task5DoneAt = datetime.now() - timedelta(minutes=1)
+    task6DoneAt = datetime.now() - timedelta(minutes=120)
+    expectedOld = [
+        Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
+        Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt)
+    ]
+    inputTasks = {
+        Status.ToDo: [],
+        Status.Done: [
+            Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
+            Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
+            Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
+            Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt),
+            Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=task5DoneAt),
+            Task(6, "I did something else too", status=Status.Done, last_modified=task6DoneAt)
+        ],
+        Status.Doing: []
+    }
 
-# def test_toggle_defaults_off
+    # When
+    vm = ViewModel(inputTasks)
 
-# def test_can_toggle_show_all
+    # Then
+    assert vm.older_done_items == expectedOld
