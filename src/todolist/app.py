985b09efc024bf05064039c68845e3006cba00db
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-07-22 22:00:04 +0100

    module3: create_app function

--- src/todolist/app.py
+++ src/todolist/app.py
@@ -2,25 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for
 from .TrelloApi import TrelloApi
 from .ViewModel import ViewModel
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route('/')
-def index():
-    tasks = TrelloApi.get_items()
-    item_view_model = ViewModel(tasks)
-    return render_template('index.html', data=item_view_model)
+    @app.route('/')
+    def index():
+        tasks = TrelloApi.get_items()
+        item_view_model = ViewModel(tasks)
+        return render_template('index.html', data=item_view_model)
 
-@app.route('/task', methods=['POST'])
-def addTask():
-    title = request.form.get('title')
-    description = request.form.get('description')
-    TrelloApi.add_item(title, description)
-    return redirect('/')
+    @app.route('/task', methods=['POST'])
+    def addTask():
+        title = request.form.get('title')
+        description = request.form.get('description')
+        TrelloApi.add_item(title, description)
+        return redirect('/')
 
-@app.route('/task/<id>', methods=['PUT', 'POST'])
-def completeItem(id):
-    TrelloApi.mark_done(id)
-    return redirect('/')
+    @app.route('/task/<id>', methods=['PUT', 'POST'])
+    def completeItem(id):
+        TrelloApi.mark_done(id)
+        return redirect('/')
 
-if __name__ == '__main__':
-    app.run()
+    if __name__ == '__main__':
+        app.run()
+
+    return app
