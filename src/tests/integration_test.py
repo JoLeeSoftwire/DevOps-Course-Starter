f07b05b8625bc60be142642ef1d71947013a51af
JoLeeSoftwire <Jo.Lee@softwire.com>
2020-07-23 21:37:53 +0100

    module3: assertion in integration tests (broken env values)

--- src/tests/integration_test.py
+++ src/tests/integration_test.py
@@ -20,3 +20,15 @@ def client():
 def test_index_page(monkeypatch, client):    
     monkeypatch.setattr(requests, "get", lambda endpoint, params: MockReturn(endpoint, params))
     response = client.get('/')
+    print(response)
+    assert "make it give a sensible message if a card is deleted" in response
+    # assert "ame release" in response
+    # assert "and some testing?" in respose
+    # assert "task with no desc" in response
+    # assert "test card" in response
+    # assert "another task" in response
+    # assert "with a description" in response
+    # assert "I'll finish it later" in response
+    # assert "eventually" in response
+    # assert "half way through this" in response
+
