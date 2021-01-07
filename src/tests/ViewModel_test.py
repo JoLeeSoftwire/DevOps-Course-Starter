f4fd41b1b240a8b871d0cb7f48d16aef51bafb3e
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-01-07 09:10:24 +0000

    Revert "module3pt2: viewmodel tests not time depdndent"
    This reverts commit 9afdcfbb8447f79134e65820a909e6d994b9dadd.

--- src/tests/ViewModel_test.py
+++ src/tests/ViewModel_test.py
@@ -4,29 +4,31 @@ from todolist.ViewModel import ViewModel
 from todolist.Task import Task, Status
 
 dateformat = "%Y-%m-%dT%H:%M:%S.%fZ"
-dummyNotToday = datetime.strptime("2020-02-18T08:15:27.243860Z", dateformat)
-dummyToday = datetime.strptime("2020-06-14T09:17:20.141163Z", dateformat)
 
 ### TODO: rewrite these to not fail for the first 2 hours of the day
-def test_correct_recent_done_items(monkeypatch):
+def test_correct_recent_done_items():
     # Given
-    monkeypatch.setattr(Task, "touchedOn", lambda self, date: self.last_modified == dummyToday)
-
+    task1DoneAt = datetime.now() - timedelta(minutes=15)
+    task2DoneAt = datetime.now() - timedelta(minutes=10)
+    task3DoneAt = datetime.strptime("2020-01-08T08:15:27.243860Z", dateformat)
+    task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
+    task5DoneAt = datetime.now() - timedelta(minutes=1)
+    task6DoneAt = datetime.now() - timedelta(minutes=120)
     expectedRecent = [
-        Task(1, 'I did this', status=Status.Done, last_modified=dummyToday),
-        Task(2, "I did this too", status=Status.Done, last_modified=dummyToday),
-        Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=dummyToday),
-        Task(6, "I did something else too", status=Status.Done, last_modified=dummyToday)
+        Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
+        Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
+        Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=task5DoneAt),
+        Task(6, "I did something else too", status=Status.Done, last_modified=task6DoneAt)
     ]
     inputTasks = {
         Status.ToDo: [],
         Status.Done: [
-            Task(1, 'I did this', status=Status.Done, last_modified=dummyToday),
-            Task(2, "I did this too", status=Status.Done, last_modified=dummyToday),
-            Task(3, "another thing I did", status=Status.Done, last_modified=dummyNotToday),
-            Task(4, "oh and another thing", status=Status.Done, last_modified=dummyNotToday),
-            Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=dummyToday),
-            Task(6, "I did something else too", status=Status.Done, last_modified=dummyToday)
+            Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
+            Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
+            Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
+            Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt),
+            Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=task5DoneAt),
+            Task(6, "I did something else too", status=Status.Done, last_modified=task6DoneAt)
         ],
         Status.Doing: []
     }
@@ -37,23 +39,27 @@ def test_correct_recent_done_items(monkeypatch):
     # Then
     assert vm.recent_done_items == expectedRecent
 
-def test_show_all_if_more_than_five_and_toggle_on(monkeypatch):
+def test_show_all_if_more_than_five_and_toggle_on():
     # Given
-    monkeypatch.setattr(Task, "touchedOn", lambda self, date: self.last_modified == dummyToday)
-
+    task1DoneAt = datetime.now() - timedelta(minutes=15)
+    task2DoneAt = datetime.now() - timedelta(minutes=10)
+    task3DoneAt = datetime.strptime("2020-01-08T08:15:27.243860Z", dateformat)
+    task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
+    task5DoneAt = datetime.now() - timedelta(minutes=1)
+    task6DoneAt = datetime.now() - timedelta(minutes=120)
     expectedOld = [
-        Task(3, "another thing I did", status=Status.Done, last_modified=dummyNotToday),
-        Task(4, "oh and another thing", status=Status.Done, last_modified=dummyNotToday)
+        Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
+        Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt)
     ]
     inputTasks = {
         Status.ToDo: [],
         Status.Done: [
-            Task(1, 'I did this', status=Status.Done, last_modified=dummyToday),
-            Task(2, "I did this too", status=Status.Done, last_modified=dummyToday),
-            Task(3, "another thing I did", status=Status.Done, last_modified=dummyNotToday),
-            Task(4, "oh and another thing", status=Status.Done, last_modified=dummyNotToday),
-            Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=dummyToday),
-            Task(6, "I did something else too", status=Status.Done, last_modified=dummyToday)
+            Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
+            Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
+            Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
+            Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt),
+            Task(5, "a fifth thing I have doned", status=Status.Done, last_modified=task5DoneAt),
+            Task(6, "I did something else too", status=Status.Done, last_modified=task6DoneAt)
         ],
         Status.Doing: []
     }
