990dc540ae87f833dfe141988a5416c431a82971
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-01-07 09:22:46 +0000

    module3pt2: fis times in tests

--- src/tests/ViewModel_test.py
+++ src/tests/ViewModel_test.py
@@ -1,5 +1,5 @@
 import pytest
-from datetime import datetime, timedelta
+from datetime import datetime
 from todolist.ViewModel import ViewModel
 from todolist.Task import Task, Status
 
@@ -8,12 +8,12 @@ dateformat = "%Y-%m-%dT%H:%M:%S.%fZ"
 ### TODO: rewrite these to not fail for the first 2 hours of the day
 def test_correct_recent_done_items():
     # Given
-    task1DoneAt = datetime.now() - timedelta(minutes=15)
-    task2DoneAt = datetime.now() - timedelta(minutes=10)
+    task1DoneAt = datetime.now().replace(minute=13, hour=1)
+    task2DoneAt = datetime.now().replace(minute=0, hour=9)
     task3DoneAt = datetime.strptime("2020-01-08T08:15:27.243860Z", dateformat)
     task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
-    task5DoneAt = datetime.now() - timedelta(minutes=1)
-    task6DoneAt = datetime.now() - timedelta(minutes=120)
+    task5DoneAt = datetime.now().replace(minute=59, hour=23)
+    task6DoneAt = datetime.now().replace(minute=45, hour=5)
     expectedRecent = [
         Task(1, 'I did this', status=Status.Done, last_modified=task1DoneAt),
         Task(2, "I did this too", status=Status.Done, last_modified=task2DoneAt),
@@ -41,12 +41,12 @@ def test_correct_recent_done_items():
 
 def test_show_all_if_more_than_five_and_toggle_on():
     # Given
-    task1DoneAt = datetime.now() - timedelta(minutes=15)
-    task2DoneAt = datetime.now() - timedelta(minutes=10)
+    task1DoneAt = datetime.now().replace(minute=13, hour=1)
+    task2DoneAt = datetime.now().replace(minute=0, hour=9)
     task3DoneAt = datetime.strptime("2020-01-08T08:15:27.243860Z", dateformat)
     task4DoneAt = datetime.strptime("2020-06-29T09:17:20.141163Z", dateformat)
-    task5DoneAt = datetime.now() - timedelta(minutes=1)
-    task6DoneAt = datetime.now() - timedelta(minutes=120)
+    task5DoneAt = datetime.now().replace(minute=59, hour=23)
+    task6DoneAt = datetime.now().replace(minute=45, hour=5)
     expectedOld = [
         Task(3, "another thing I did", status=Status.Done, last_modified=task3DoneAt),
         Task(4, "oh and another thing", status=Status.Done, last_modified=task4DoneAt)
