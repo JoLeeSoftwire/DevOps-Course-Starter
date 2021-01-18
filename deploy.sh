ca58bee8c87d44ae36441ff6089ef3fd880eb376
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-01-18 17:22:09 +0000

    module8pt2: move jol-todoapp into env var

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-# docker login --username=joleesoftwire --password=$HEROKU_API_KEY registry.heroku.com
 docker push joleesoftwire/todoapp:$TRAVIS_BRANCH
-docker tag joleesoftwire/todoapp registry.heroku.com/jol-todoapp/web
-docker push registry.heroku.com/jol-todoapp/web
-heroku container:release --app jol-todoapp web
+docker tag joleesoftwire/todoapp registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release --app $HEROKU_APP_NAME web
