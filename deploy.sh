76a9138ddbbfab7f09c880c5973201f9dfa2caa6
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-02-02 16:09:29 +0000

    module8: fix shebang

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-# docker push joleesoftwire/todoapp:$TRAVIS_BRANCH
-# docker tag joleesoftwire/todoapp registry.heroku.com/$HEROKU_APP_NAME/web
-# docker push registry.heroku.com/$HEROKU_APP_NAME/web
-# heroku container:release --app $HEROKU_APP_NAME web
+echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
+docker push joleesoftwire/todoapp:$TRAVIS_BRANCH
+docker tag joleesoftwire/todoapp registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release --app $HEROKU_APP_NAME web
