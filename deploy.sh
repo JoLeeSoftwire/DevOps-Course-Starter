bb4bd232c0532c54861298534c0e78bfcf374225
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-02-02 16:24:20 +0000

    module8: dont repeat login

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 docker push joleesoftwire/todoapp:$TRAVIS_BRANCH
 docker tag joleesoftwire/todoapp registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
