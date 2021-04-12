72c2bdfe88dd786c645d214e189cb1d5edc596ac
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-04-12 10:57:47 +0100

    module10: WIP trying to manage redirect

--- src/todolist/app.py
+++ src/todolist/app.py
@@ -1,10 +1,15 @@
-import requests
-import os
-from random import randint
+import requests, os
+# from random import randint
 from flask import Flask, render_template, request, redirect, url_for
 from .DbCommunicator import DbCommunicator
 from .ViewModel import ViewModel
-from flask_login import LoginManager, login_required
+from .User import User
+from flask_login import LoginManager, login_required, login_user
+from oauthlib.oauth2 import WebApplicationClient
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+client = WebApplicationClient(CLIENT_ID)
 
 def create_app():
     app = Flask(__name__)
@@ -12,21 +17,46 @@ def create_app():
 
     @login_manager.unauthorized_handler
     def unauthenticated():
-        CLIENT_ID = os.environ.get('CLIENT_ID')
-        CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
         endpoint = "https://github.com/login/oauth/authorize"
-        state = randomString(12)
-        github_url = endpoint+"?client_id="+CLIENT_ID+"&state="+state
+        # state = randomString(12)
+        github_url = client.prepare_request_uri(endpoint)
 
-        # redirect = requests.get(endpoint, params=params)
         return redirect(github_url)
-        # prepare_request_uri()
 
     @login_manager.user_loader
     def load_user(user_id):
-        return None
+        user = User(user_id)
+        return user
 
     login_manager.init_app(app)
+
+    @app.route('/login/callback')
+    def login():
+        code = request.args.get('code')
+        endpoint = "https://github.com/login/oauth/access_token"
+        params = {
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "code": code
+        }
+        headers = {
+            "Accept": "application/json"
+        }
+        response = requests.post(endpoint, params=params, headers=headers)
+        token_json = response.json()
+        token = token_json['access_token']
+        
+        user_endpoint = "https://api.github.com/user"
+        auth_header = {
+            "Authorization": "Bearer "+token
+        }
+        user_details = requests.get(user_endpoint, headers=auth_header)
+        user = User(user_details['user_id'])
+        
+        success = login_user(user)
+        
+        return redirect(url_for('index'))
+
 
     @app.route('/')
     @login_required
@@ -47,12 +77,12 @@ def create_app():
         DbCommunicator.mark_done(id)
         return redirect('/')
 
-    def randomString(len):
-        random_string = ''
-        for _ in range(len):
-            random_integer = randint(97, 97+26-1)
-            random_string += (chr(random_integer))
-        return random_string
+    # def randomString(len):
+    #     random_string = ''
+    #     for _ in range(len):
+    #         random_integer = randint(97, 97+26-1)
+    #         random_string += (chr(random_integer))
+    #     return random_string
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0')
