258a4433305268102be61af742697d149bdb0bff
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-05-11 17:39:39 +0100

    module10: create roles

--- src/todolist/User.py
+++ src/todolist/User.py
@@ -1,5 +1,14 @@
 from flask_login import UserMixin
+import enum
+
+class Role(enum.Enum):
+    Reader = 0
+    Writer = 1
+
 
 class User(UserMixin):
     def __init__(self, id):
         self.id = id
+
+    def role(self):
+        return Role.Writer if (self.id == "57954007") else Role.Reader
