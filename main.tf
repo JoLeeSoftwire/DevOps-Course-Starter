4722af79a7b5476b11d8a79ae92a2da380d031d6
JoLeeSoftwire <Jo.Lee@softwire.com>
2021-06-18 16:52:28 +0100

    module12: first resource creation

--- main.tf
+++ main.tf
@@ -14,3 +14,30 @@ provider "azurerm" {
 data "azurerm_resource_group" "main" {
     name = "SoftwirePilot_JoLee_ProjectExercise"
 }
+
+resource "azurerm_app_service_plan" "main" {
+    name = "terraformed-asp"
+    location = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    kind = "Linux"
+    reserved = true
+
+    sku {
+        tier = "Basic"
+        size = "B1"
+        }
+}
+
+resource "azurerm_app_service" "main" {
+    name = "jol-todo-webapp-tf"
+    location = data.azurerm_resource_group.main.location
+    resource_group_name = data.azurerm_resource_group.main.name
+    app_service_plan_id = azurerm_app_service_plan.main.id
+    site_config {
+        app_command_line = ""
+        linux_fx_version = "DOCKER|appsvcsample/pythonhelloworld:latest"
+    }
+    app_settings = {
+        "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+    }
+}
